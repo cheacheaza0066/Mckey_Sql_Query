4f6b4fafc0a05faff2e5d2fa3a731fcb5af711c6
sushijawow <sushijawow@gmail.com>
2024-05-02 11:03:34 +0700

    update

--- Rawdata/Rawdata_CO2_Usage.sql
+++ Rawdata/Rawdata_CO2_Usage.sql
@@ -17,7 +17,7 @@ FROM (
       ,[Line5]
       ,[Line6]
       ,[Description]
-        CASE WHEN [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] IS NULL THEN 0 ELSE [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] END AS [CO2_usage]
+        CASE WHEN [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] IS NULL THEN 0 ELSE [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] END AS [CO2_usagel]
        
     FROM [Test].[dbo].[RawData_CO2_Usage]
    -- where [Rawdata_Running_Hr_Plant3].[Date] between :Start and :Finish
