e65e3d89d056944443b488232b375817102eceaa
sushijawow <sushijawow@gmail.com>
2024-05-02 11:47:52 +0700

    kk

--- Rawdata/Rawdata_CO2_Usage.sql
+++ Rawdata/Rawdata_CO2_Usage.sql
@@ -19,6 +19,6 @@ FROM (
       ,[Description]
         CASE WHEN [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] IS NULL THEN 0 ELSE [Meter_CO2] - LEAD([Meter_CO2]) OVER (ORDER BY [Date]) + [Fill_Up_CO2] END AS [CO2_usage]
        
-    FROM [Test].[dbo].[RawData_CO2_Usage]
+    FROM [Test].[dbo].[RawData_CO2_Usage]l
    -- where [Rawdata_Running_Hr_Plant3].[Date] between :Start and :Finish
 ) AS Subquery
