c7a116e0a33b6edb800d65f19aafa6c13cf951f5
Thanin <chea14chainat@gmail.com>
2024-07-04 16:18:31 +0700

    summary

--- Summary/Summary.sql
+++ Summary/Summary.sql
@@ -14,7 +14,16 @@ SELECT
 	 (SELECT CAST(Sum(wtp.Total_waste_water) AS INT)
      FROM [ISMPALI].[dbo].[ut_sus_rpt_total_wtp_wwtp_usage] as wtp
      WHERE Date between @StartDate and @EndDate) as Total_Waste_Water_volumn,
+
+	  (SELECT CAST(Sum(mdb1.PEA_Meter) AS INT)
+     FROM [ISMPALI].[dbo].[ut_sus_rpt_electric_usage_mdb1] as mdb1
+     WHERE Date between @StartDate and @EndDate) as Total_PEA_Meter,
+
     
     (SELECT CAST(Sum(cng.[SCM_CNG_usage]) AS INT)
      FROM [ISMPALI].[dbo].[ut_sus_rpt_cng_usage] as cng
-     WHERE Date between @StartDate and @EndDate) as SCM_CNG_usage
+     WHERE Date between @StartDate and @EndDate) as CNG_Usage_Run_A_Run_B_Meter,
+	 
+	 (SELECT CAST(Sum(co2.[CO2_usage]) AS INT)
+     FROM [ISMPALI].[dbo].[ut_sus_rpt_co2_usage] as co2
+     WHERE Date between @StartDate and @EndDate) as CO2_usage
